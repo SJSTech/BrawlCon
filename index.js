96ee5f184d3fd8c9abdf09872d056ff45e2bef95
Techboy9 <76917634+Con-Co@users.noreply.github.com>
2021-01-12 09:05:51 -0600

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ client.on("ready", () => {
 client.on("guildCreate", guild => {
   // This event triggers when the bot joins a guild.
   console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-  client.user.setActivity(`Now in ${client.guilds.cache.size} servers!`);
+  client.user.setActivity(`Now in ${client.guilds.cache.size} servers | -bc help`);
 });
 
 client.on("guildDelete", guild => {
