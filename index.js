4ef94bb9f43f4690537f53c14d427f86d9fc55a1
Techboy9 <76917634+Con-Co@users.noreply.github.com>
2021-01-13 21:11:02 -0600

    Update index.js

--- index.js
+++ index.js
@@ -20,19 +20,9 @@ client.on("ready", () => {
  // client.user.setActivity(`Serving ${client.guilds.cache.size} servers`);
  //client.user.setActivity(`Con Co. | -bc help`);
  //client.user.setPresence({ Con Co. | -bc help: { name: 'LISTENING' }, status: 'idle' });
-
-  const activities_list = [
-    "with the &help command.", 
-    "with the developers console",
-    "with some code", 
-    "with JavaScript"
-    ]; // creates an arraylist containing phrases you want your bot to switch through.
-
-bot.on('ready', () => {
-    setInterval(() => {
-        const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
-        bot.user.setActivity(activities_list[index]); // sets bot's activities to one of the phrases in the arraylist.
-    }, 10000); // Runs this every 10 seconds.
+ stream client.user.setActivity("Con Co. | -bc help", {
+  type: 'STREAMING',
+  url: "https://www.twitch.tv/wel3comco"
 });
 });
 
