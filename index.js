0d4c12caf2968f8df884a9a0b1d0d55083b4c639
Techboy9 <76917634+Con-Co@users.noreply.github.com>
2021-01-14 16:22:22 -0600

    Update index.js

--- index.js
+++ index.js
@@ -18,13 +18,14 @@ client.on("ready", () => {
   // Example of changing the bot's playing game to something useful. `client.user` is what the
   // docs refer to as the "ClientUser".
  // client.user.setActivity(`Serving ${client.guilds.cache.size} servers`);
- client.user.setActivity(`Con Co. | -bc help`);
+ //client.user.setActivity(`Con Co. | -bc help`);
  //client.user.setActivity('Con Co. | -bc help', { type: 'LISTENING' });
  //client.user.setActivity('Brawl Talk | -bc help', { type: 'WATCHING' });
  //client.user.setActivity('Brawl Stars | -bc help');
  //client.user.setStatus('dnd');
  //client.user.setPresence({ activity: { name: 'In Maintenance | Con Co.' }, status: 'dnd' });
  //client.user.setPresence({ activity: { name: 'Downtime | Con Co.' }, status: 'idle' });
+ client.user.setPresence({ activity: { name: 'Offline' }, status: 'invisible' });
 });
 
 client.on("guildCreate", guild => {
