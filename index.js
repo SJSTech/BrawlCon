95e97b8c660d239ff4a78d8756f21ed27e4418b3
Techboy9 <76917634+Con-Co@users.noreply.github.com>
2021-01-15 10:38:30 -0600

    Update index.js

--- index.js
+++ index.js
@@ -59,6 +59,17 @@ client.on("message", async message => {
 
   // Let's go with a few common example commands! Feel free to delete or change those.
   
+  if (command === 'prefix') {
+	// if there's at least one argument, set the prefix
+	if (args.length) {
+		await prefixes.set(message.guild.id, args[0]);
+		return message.channel.send(`Successfully set prefix to \`${args[0]}\``);
+	}
+
+	return message.channel.send(`Prefix is \`${await prefixes.get(message.guild.id) || globalPrefix}\``);
+}
+    
+    
   if(command === "help") {
 const embed = new Discord.MessageEmbed()
   .setTitle("Commands")
