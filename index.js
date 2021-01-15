50b7cd0c3683d18e28150729df8105472db64872
Techboy9 <76917634+Con-Co@users.noreply.github.com>
2021-01-14 20:51:14 -0600

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const activities_list = [
 client.on('ready', () => {
     setInterval(() => {
         const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
-        client.user.setActivity(activities_list[index], { type: 'STREAMING' }); // sets bot's activities to one of the phrases in the arraylist.
+        client.user.setActivity(activities_list[index], { type: 'PLAYING' }); // sets bot's activities to one of the phrases in the arraylist.
     }, 10000); // Runs this every 10 seconds.
 });
 
