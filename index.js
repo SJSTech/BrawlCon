4bc9078ca2652252697f60469b7f36ce17d7c66c
Techboy9 <76917634+Con-Co@users.noreply.github.com>
2021-01-15 10:50:57 -0600

    Update index.js

--- index.js
+++ index.js
@@ -59,7 +59,38 @@ client.on("message", async message => {
 
   // Let's go with a few common example commands! Feel free to delete or change those.
   
-  if (command === 'prefix') {
+ client.on('message', async message => {
+	if (message.author.bot) return;
+
+	let args;
+	// handle messages in a guild
+	if (message.guild) {
+		let prefix;
+
+		if (message.content.startsWith(globalPrefix)) {
+			prefix = globalPrefix;
+		} else {
+			// check the guild-level prefix
+			const guildPrefix = await prefixes.get(message.guild.id);
+			if (message.content.startsWith(guildPrefix)) prefix = guildPrefix;
+		}
+
+		// if we found a prefix, setup args; otherwise, this isn't a command
+		if (!prefix) return;
+		args = message.content.slice(prefix.length).trim().split(/\s+/);
+	} else {
+		// handle DMs
+		const slice = message.content.startsWith(globalPrefix) ? globalPrefix.length : 0;
+		args = message.content.slice(slice).split(/\s+/);
+	}
+
+	// get the first space-delimited argument after the prefix as the command
+	const command = args.shift().toLowerCase();
+});
+	
+	
+	
+   if (command === 'prefix') {
 	// if there's at least one argument, set the prefix
 	if (args.length) {
 		await prefixes.set(message.guild.id, args[0]);
