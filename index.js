18b273f5dc25d18d243c508d05aaa494b5e54603
Techboy9 <76917634+Con-Co@users.noreply.github.com>
2021-01-19 20:06:55 -0600

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,12 @@ client.on('ready', () => {
     setInterval(() => {
         const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
         client.user.setActivity(activities_list[index], { type: 'PLAYING' }); // sets bot's activities to one of the phrases in the arraylist.
-    }, 300000); // Runs this every 10 seconds.
+    }, 300000); // Runs this every 5 minutes.
+    
+    
+      // Custom Welcome reference
+    
+   welcome(client);
 });
 
 
@@ -58,11 +63,6 @@ client.on("message", async message => {
   // args = ["Is", "this", "the", "real", "life?"]
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
-  
-    
-  // Custom Welcome reference
-    
-   welcome(client);
 
   // Let's go with a few common example commands! Feel free to delete or change those!
     
