f42029aa15e1adbecd0f48db674c507b58e7a236
Techboy9 <76917634+Con-Co@users.noreply.github.com>
2021-01-19 21:15:57 -0600

    Update welcome.js

--- welcome.js
+++ welcome.js
@@ -1,11 +1,10 @@
 module.exports = (client) => {
   const channelId = '790348592085991504' //welcome channel
-  //const targetChannelId = '782022655402311700' //rules channel
   
   client.on('guildMemberAdd', (member) => {
     console.log(member)
     
-    const message = 'Welcome to the server <@${member.id}>! We hope you have fun and enjoy your stay!
+    const message = 'Welcome to the server <@${member.id}>! We hope you have fun and enjoy your stay!'
     
     const channel = member.guild.channels.cache.get(channelId)
     channel.send(message)
