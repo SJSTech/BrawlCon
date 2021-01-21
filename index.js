aacb9c8def498fa8869f0af90b7c961bcff3da1d
Techboy9 <76917634+Con-Co@users.noreply.github.com>
2021-01-21 14:58:18 -0600

    Update index.js

--- index.js
+++ index.js
@@ -29,11 +29,6 @@ client.on('ready', () => {
     }, 300000); // Runs this every 5 minutes.
    
 
-	
-	welcome(client);
-});
-
-
 client.on("guildCreate", guild => {
   // This event triggers when the bot joins a guild.
  // console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
