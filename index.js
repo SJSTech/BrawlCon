e4218fa74e03b90a065973e6688898da761c796a
Techboy9 <76917634+Con-Co@users.noreply.github.com>
2021-01-24 17:35:25 -0600

    Update index.js

--- index.js
+++ index.js
@@ -13,22 +13,20 @@ const welcome = require ('./welcome');
 // config.token contains the bot's token
 // config.prefix contains the message prefix.
 
-client.on('ready', () => {
-  // This event will run if the bot starts, and logs in, successfully.
-  console.log(Bot has started, with ${client.users.cache.size} users, in ${client.channels.cache.size} channels of ${client.guilds.cache.size} guilds.);
-  // Example of changing the bot's playing game to something useful. client.user is what the
-  // docs refer to as the "ClientUser".
- // client.user.setActivity(Serving ${client.guilds.cache.size} servers);
- //client.user.setActivity(Con Co. | -bc help);
- //client.user.setActivity('Con Co. | -bc help', { type: 'LISTENING' });
- //client.user.setActivity('Brawl Talk | -bc help', { type: 'WATCHING' });
- //client.user.setActivity('Brawl Stars | -bc help');
- //client.user.setStatus('dnd');
- client.user.setPresence({ activity: { name: 'In Maintenance | Con Co.' }, status: 'dnd' });
- //client.user.setPresence({ activity: { name: 'Downtime | Con Co.' }, status: 'idle' });
- //client.user.setPresence({ activity: { name: 'Offline' }, status: 'dnd' });
-});
+const activities_list = [ 
+    "Brawl Ball | -bc help",
+    "Solo SD | -bc help", 
+    "Con Co. | -bc help",
+    "Brawl Stars | -bc help",
+    "Virus 8-bit | -bc help",
+    "Update Hype! | -bc help"
+    ]; // creates an arraylist containing phrases you want your bot to switch through.
 
+client.on('ready', () => {
+    setInterval(() => {
+        const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
+        client.user.setActivity(activities_list[index], { type: 'PLAYING' }); // sets bot's activities to one of the phrases in the arraylist.
+    }, 300000); // Runs this every 5 minutes.
 
 
 
