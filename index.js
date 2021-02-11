d7d9221ec7801d62acac8999facc15b634d17d06
Techboy9 <76917634+Con-Co@users.noreply.github.com>
2021-02-10 18:36:13 -0600

    Update index.js

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ message.channel.send("dab");
 	if(command === "support") {
 message.reply("Staff will be with you shortly please describe your question or concern in another message. Thanks!");
 const user = client.users.cache.get('672930250577018899');
-user.send("<@" " + message.author.id + " ">" " just requested a ticket in the support center!");
+user.send("just requested a ticket in the support center!");
 	}
 	
 	
