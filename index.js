c2dc02d4ccc761bf583b37eb792bc81e1dee6064
Techboy9 <76917634+Con-Co@users.noreply.github.com>
2021-03-02 12:52:17 -0600

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const client = new Discord.Client();
 
 // Here we load the config.json file that contains our token and our prefix values. 
 const config = require("./package.json");
-const welcome = require("./welcome.js");
 // config.token contains the bot's token
 // config.prefix contains the message prefix.
 
@@ -23,7 +22,6 @@ client.on("ready", () => {
  //client.user.setPresence({ activity: { name: 'Con Co. | Maintenance in Progress!' }, status: 'idle' });
  //client.user.setPresence({ activity: { name: 'Restarting Systems' }, status: 'dnd' });
 // const request = require('request');
-welcome(client)
 });	
 	
 	
