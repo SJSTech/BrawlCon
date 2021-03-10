d085397bd29d3af309a0e0ba2e31e0d700c8ab21
Techboy9 <76917634+Con-Co@users.noreply.github.com>
2021-03-09 18:38:34 -0600

    Update index.js

--- index.js
+++ index.js
@@ -125,7 +125,14 @@ message.channel.send("dab");
  }
 	
 	
-	
+	if(command === "ping") {
+    // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
+    // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
+    const m = await message.channel.send("Loading...");
+    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
+  message.channel.startTyping();
+  message.channel.stopTyping();
+  }
 	
 
 
