45a6975a2ec8d162544d69f30f0d10451495a60d
Techboy9 <76917634+Con-Co@users.noreply.github.com>
2021-03-23 19:47:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Discord = require("discord.js");
 // This is your client. Some people call it `bot`, some people call it `self`, 
 // some might call it `cootchie`. Either way, when you see `client.something`, or `bot.something`,
 // this is what we're refering to. Your client.
-//const client = new Discord.Client();
+const client = new Discord.Client();
 
 // Here we load the config.json file that contains our token and our prefix values. 
 const config = require("./package.json");
