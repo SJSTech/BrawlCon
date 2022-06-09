f9ef6995a14a6a2ba09cabbfdb9f832ce3791c3d
Techboy9 <76917634+Con-Co@users.noreply.github.com>
2022-06-09 13:30:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 // Load up the discord.js library
 const Discord = require("discord.js");
 // const token = require("./token.js");
-
+const client = new Client({ intents: [Intents.FLAGS.GUILDS], ws: { properties: { $browser: "Discord iOS" }} });
 // This is your client. Some people call it `bot`, some people call it `self`, 
 // some might call it `cootchie`. Either way, when you see `client.something`, or `bot.something`,
 // this is what we're refering to. Your client.
 const client = new Discord.Client();
+
 
 // Here we load the config.json file that contains our token and our prefix values. 
 const config = require("./package.json");
